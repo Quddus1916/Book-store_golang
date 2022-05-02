9a717c8b04deb00ee0b9359d29671057c77a46cc
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-02 01:06:49 -0500

    handle->Handle

--- pkg/routes/book_Store_Routes.go
+++ pkg/routes/book_Store_Routes.go
@@ -6,11 +6,11 @@ import(
 )
 
 var registerBookStoreRoutes = func (router *mux.Router){
-	router.handleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.handleFunc("/book/",controllers.GetBook).Methods("GET")
-	router.handleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
-	router.handleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
-	router.handleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
 
 
 }
