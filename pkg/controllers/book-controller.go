8d4ebc49402f12fe44fc233a8e58cefab2efbb5a
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-02 02:02:47 -0500

    getbook controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,2 +1,43 @@
 package controllers
 
+import(
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+	"github.com/Quddus1916/Book-store_golang/pkg/utils"
+	"github.com/Quddus1916/Book-store_golang/pkg/models"
+)
+var NewBook  models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res,_ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+	
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+	
+}
+
+
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+
+	
+}
+
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+	
+}
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+	
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -21,3 +21,28 @@ func init(){
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+func  CreateBook(b *Book) *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func  GetBookById(id int64) (*Book ,*gorm.DB){
+	var BookById Book
+	db:= db.Where("ID=?",id).Find(&BookById)
+	return &BookById ,db
+ 
+}
+
+func DeleteBook(id int64) Book{
+	var Book Book
+	 db.Where("ID=?",id).Delete(Book)
+	return Book
+
+}
